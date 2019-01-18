b6d276a9eb240216d2c64d80102f1c1837c39096
Sébastien Aubert <sebastien.aubert@heig-vd.ch>
2019-01-18 09:17:20 +0100

    Structuration

--- js/algo.js
+++ js/algo.js
@@ -1,26 +1,8 @@
-//TODO:Choix de couches référence, ajout de data(ensoleillement, avalanche, Fonction de tri ( mode freeride, ensoleillement etc.)
-
-/**
- * Create a marker and save all WeatherData needed in properties
- * @param result
- * @param key
- * @returns {ol.Feature}
- */
-function createMarker(result, key) {
-    let weatherData = result.currently
-    var iconFeature = new ol.Feature({
-        geometry: new ol.geom.Point([key.long, key.lat]),
-        name: key.name,
-        temperature: weatherData.temperature,
-        windSpeed: weatherData.windSpeed, // m/s
-        visibility: weatherData.visibility, // KM
-        precipitation: weatherData.precipIntensity, // mm/h
-        style: iconSelectStyle
-    });
-    return iconFeature;
-}
-
 $(document).ready(function () {
+    //Kaufy**************************************************************************************
+    /**
+     * Map and layers Setup
+     */
     var map;
     map = new ol.Map({
         view: new ol.View({
@@ -41,39 +23,111 @@ $(document).ready(function () {
             }
         })
     });
-
     var layerBingMaps = new ol.layer.Tile({
         source: new ol.source.BingMaps({
             key: 'AqE05oJsq-bWa50FPOW2S0eQm9Oqqygc1VTi_WPhUIoKR_-jgA559CRbfndgWAIz',
             imagerySet: 'Road'
         })
     });
-
     map.addLayer(layerBingMaps);
     map.addLayer(layerOSM);
-    var mapInteraction = new ol.interaction.Select({
-        condition: ol.events.condition.singleClick,
-        // the interactive layers on which the selection is possible (they may be more than one)
-        layers: [layerBingMaps]
-    });
-    map.addInteraction(mapInteraction);
-    mapInteraction.on('select', function (e) {
-        if (e.selected.length > 0) {
-            $('.spanInfo').hide();
-        }
-    });
-
-//Configuration of the map view
-//Configuration n°2 of the map view
+    /**
+     * View
+     * @type {ol.View}
+     */
     var v2 = new ol.View({projection: "EPSG:4326"});
     var cbox = [6.926040, 46.398030]; // Focus on Villeneuve
     v2.setCenter(cbox);
     v2.setZoom(10);
     map.setView(v2);
-//------------------------------
-    var labelFeatures = [];
-//Get ski Stations and draw icons and labels
+    var iconSelectStyle = new ol.style.Style({
+        image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
+            anchor: [0.5, 46],
+            anchorXUnits: 'fraction',
+            anchorYUnits: 'pixels',
+            opacity: 0.75,
+            src: '../images/placeholder.png'
+        }))
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //SEB**************************************************************************************
+    /**
+     * Get all the ski stations and draw them
+     */
     setTimeout(async () => {
+        var labelFeatures = [];
         //Retrieve all GeoData from darksky API
         let response2 = await $.getJSON(`https://sebibigbob8.carto.com/api/v2/sql/?q=select * from stations`);
         let keys = Object.values(response2.rows);
@@ -90,6 +144,7 @@ $(document).ready(function () {
             source: vectorSource,
         });
         map.addLayer(labelLayer);
+
         var selectInteraction = new ol.interaction.Select({
             condition: ol.events.condition.singleClick,
             // the interactive layers on which the selection is possible (they may be more than one)
@@ -118,18 +173,30 @@ $(document).ready(function () {
     }, 1000);
 
     /**
-     * Changement de couche
+     * LayerSwitcher
      */
     var layerSwitcher = new ol.control.LayerSwitcher();
     map.addControl(layerSwitcher);
+    function createMarker(result, key) {
+        let weatherData = result.currently
+        var iconFeature = new ol.Feature({
+            geometry: new ol.geom.Point([key.long, key.lat]),
+            name: key.name,
+            temperature: weatherData.temperature,
+            windSpeed: weatherData.windSpeed, // m/s
+            visibility: weatherData.visibility, // KM
+            precipitation: weatherData.precipIntensity, // mm/h
+            style: iconSelectStyle
+        });
+        return iconFeature;
+    }
 
-});
-var iconSelectStyle = new ol.style.Style({
-    image: new ol.style.Icon(/** @type {olx.style.IconOptions} */ ({
-        anchor: [0.5, 46],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'pixels',
-        opacity: 0.75,
-        src: '../images/placeholder.png'
-    }))
-});
+})
+;
+
+/**
+ * Create a marker and save all WeatherData needed in properties
+ * @param result
+ * @param key
+ * @returns {ol.Feature}
+ */
