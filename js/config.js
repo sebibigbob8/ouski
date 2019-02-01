fd3cdfcbf95264b34edf642a6e1a869baadb3faf
Sébastien Aubert <sebastien.aubert@heig-vd.ch>
2019-02-01 18:32:14 +0100

    github page deploy

--- js/config.js
+++ js/config.js
@@ -63,14 +63,14 @@ var unWFS = "http://preview.grid.unep.ch/geoserver/wfs";
  * @requires lib/jquery-2.1.4.min.js
  *
  */
-stylesheetTag = '<link rel="stylesheet" href="http://openlayers.org/en/v3.20.1/css/ol.css" type="text/css">'
-document.write(stylesheetTag);
+/*stylesheetTag = '<link rel="stylesheet" href="https://openlayers.org/en/v3.20.1/css/ol.css" type="text/css">'
+document.write(stylesheetTag);*/
 
-scriptTag = '<script src="https://openlayers.org/en/v3.20.1/build/ol-debug.js" type="text/javascript"></script>';
-document.write(scriptTag);
+/*scriptTag = '<script src="https://openlayers.org/en/v3.20.1/build/ol-debug.js" type="text/javascript"></script>';
+document.write(scriptTag);*/
 
-scriptTag = '<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>';
-document.write(scriptTag);
+/*scriptTag = '<script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script>';
+document.write(scriptTag);*/
 
 /*
  * May be some useful functions
